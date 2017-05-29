b7949c21a7d703a381ae63fe7df8eca511f21ed6
eeghor <eeghor@gmail.com>
2017-05-29 15:08:29 +1000

    improved

--- wiki-artist.py
+++ wiki-artist.py
@@ -2,45 +2,53 @@ import wikipedia
 import requests
 import re
 from bs4 import BeautifulSoup
+from collections import defaultdict
 
 artists = "air deftones interpol sia editors".split()
+
+artist_dic = defaultdict
 
 for a in artists:
 
     url = wikipedia.page(a + " (band)").url
+
     r = requests.get(url).text  # response body as bytes so need get text first
 
     soup = BeautifulSoup(r, "lxml")
 
-    years_span = soup.find("span", string=re.compile('^Years'))
-    years = years_span.parent.next_sibling.next_sibling.text
-    print(years)
+    def scrape_infobox(txt2find):
 
-    try:
-        brn  = soup.find("th", string=re.compile('^Born'))
-        if not brn:
-            print('found no born')
-        else:
-            print(brn)
-            print(brn.next_sibling.next_sibling.text)
-    except:
-        pass
+        try:
+            right_th  = soup.find("th", string=re.compile('^' + txt2find))
+            if right_th:
+                res = right_th.next_sibling.next_sibling.text.lower().strip().split("\n")
+                # note that res is a list after .split()
+                if (len(res) == 1) or (txt2find == "Born"):  # if it's one-liner or Born
+                    res = res[0]
+            else:
+                res = None
+        except:
+            res = None
 
-    try:
-        mmb  = soup.find("th", string=re.compile('^Members'))
-        if not mmb:
-            print('found no members')
-        else:
-            print([l.strip().lower() for l in mmb.next_sibling.next_sibling.text.split("\n") if l.strip()])
-    except:
-        pass 
+        return res
 
+    def fill_artist_rec(artist_name):
 
-    try:
-        lab  = soup.find("th", string=re.compile('^Labels'))
-        if not lab:
-            print('found no labels')
-        else:
-            print([l.strip().lower() for l in lab.next_sibling.next_sibling.text.split("\n") if l.strip()])
-    except:
-        pass 
+        art_rec = {
+                    artist_name: 
+                        { 
+                        "born": None, 
+                        "members": [],
+                        "years_active": None,
+                        "labels": []
+                        }
+                    }
+
+        art_rec[artist_name]["born"] = scrape_infobox('Born')
+        art_rec[artist_name]["members"] = scrape_infobox('Members')
+        art_rec[artist_name]["years_active"] = scrape_infobox('Years')
+        art_rec[artist_name]["labels"] = scrape_infobox('Labels')
+
+        return art_rec
+
+    print(fill_artist_rec(a))
