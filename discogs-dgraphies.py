34f83e84e2e4e60f5c8e34baa7a42af77d65ee12
eeghor <eeghor@gmail.com>
2017-06-02 17:14:56 +1000

    almost fixed the api issues

--- discogs-dgraphies.py
+++ discogs-dgraphies.py
@@ -1,4 +1,5 @@
 import discogs_client
+from discogs_client.exceptions import HTTPError
 import pprint
 import json
 import time
@@ -28,39 +29,72 @@ collected_data = []
 
 t0 = time.time()
 
-artist_list = [a for a in list(artists.keys()) if not (set(a.split()) & {"hits", "greatest", "choir"})]
-print(len(artist_list))
+artist_list = [a for a in list(artists.keys()) if not (set(a.split()) & {"hits", "greatest", "choir", "collection"})]
 
-for j, artist_name in enumerate(artist_list[:500]):
+for j, artist_name in enumerate(artist_list[57: 60]):
 
 	print("artist {}: {}...".format(j+1, artist_name), end="")
 
 	try:
-		res = d.search(artist_name, type="artist")  # iterator containing artist objects
+		res = d.search(artist_name, type="artist") 
+		# discogs_client.models.MixedPaginatedList
+	except HTTPError:
+		print("some http error!!")
+		time.sleep(1)
+		res = d.search(artist_name, type="artist")
 	except:
-		print("failed")
-		continue  # just take next artist
+		print("some non-http exception")
+		time.sleep(1)
+		continue
+	if res.count == 0:
+		print("can\'t find")
+		continue
 
+	for artist in res.page(1):
+		try:
+			artist.releases
+			a = artist
+			break
+		except:
+			continue
+
+	#a = res.page(1)[0]
+
+	print("artist:", a)
+	print(dir(a))
+	print("refresh=", a.images)
+	print("url", a.url)
+	
 	time.sleep(1)
+
 	art_dict = defaultdict()
+	art_dict["name_variations"] = []
 
 	try:
 		art_dict["name"] = a.name.lower()
 	except:
 		art_dict["name"] = None
+
 	try:
 		art_dict["real_name"] = a.real_name.lower()
 	except:
 		art_dict["real_name"] = None
+
+	try:
+		art_dict["name_variations"] = [v.lower() for v in a.name_variations]
+	except:
+		art_dict["name_variations"] = [None]
+
+
 	try:
 		art_dict["aliases"] = [al.name.lower() for al in a.aliases]
 	except:
 		art_dict["aliases"] = [None]
+
 	art_dict["social"] = {}
 	art_dict["albums"] = []
 
-	if a.urls:
-
+	try:
 		for u in a.urls:
 			if "facebook" in u:
 				art_dict["social"].update({"facebook": u})
@@ -80,13 +114,30 @@ for j, artist_name in enumerate(artist_list[:500]):
 				if "other" not in art_dict["social"]:
 					art_dict["social"].update({"other": [u]})
 				else:
-					art_dict["social"]["other"].append(u)
+						art_dict["social"]["other"].append(u)
+	except HTTPError:
+			print("http error - NO URLS!")
+	except:
+		print("some non-http problem with urls!")
+
+
+	print("checking releases")
+
+	try:
+		a.releases
+	except:
+		print("NO RELEASES!")
+		print("failed")
+		time.sleep(1)
+		continue
+
 	# now browse releases
 	for r in a.releases:
 
 		count_nodata = 0
 		try:
 			release_data = r.data
+			time.sleep(1)
 		except AttributeError:
 			count_nodata += 1
 			print("note: {} releases without data so far!", count_nodata)
@@ -95,6 +146,7 @@ for j, artist_name in enumerate(artist_list[:500]):
 		if ((release_data["type"] in {"release", "master"}) and
 					(release_data["role"] == "Main") and
 						(len(r.tracklist) > 6)):   # then we assume it's an album
+			print("extracting another album")
 			art_dict["albums"].append(
 				{
 				"title": release_data["title"].lower(),
@@ -105,6 +157,7 @@ for j, artist_name in enumerate(artist_list[:500]):
 				})
 
 	collected_data.append(art_dict)
+	print("done with this artist")
 
 	print("ok. elapsed time: {:.0f}h {:.0f}m {:.0f}s".format(*_get_time()))
 	# wait to avoid the 'You are making requests too quickly' error
