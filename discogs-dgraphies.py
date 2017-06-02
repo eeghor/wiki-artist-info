ecc3f066911f8f6f3ba5ece1f03091b8b4f5a1e3
eeghor <eeghor@gmail.com>
2017-06-02 23:13:35 +1000

    seems all good

--- discogs-dgraphies.py
+++ discogs-dgraphies.py
@@ -7,14 +7,32 @@ import sys
 from collections import defaultdict
 
  # 60 requests per minute if auntheticated
+SLEEPSEC = 0.5
+SAVE_EVERY = 1000
 
 def _get_time():
 
 	ttup = divmod(time.time() - t0, 3600)
-	hrs = ttup[0]
 	mins, secs = divmod(ttup[1], 60)
 
-	return (hrs, mins, secs)
+	return (ttup[0], mins, secs)
+
+def _do_search(artist_name, rtry=0):
+
+	try:
+		res = d.search(artist_name, type="artist") 
+		# res is discogs_client.models.MixedPaginatedList
+	except HTTPError as e:
+		if (e.status_code == 429) and (rtry < 6):   # sending requests too quickly
+			time.sleep(3)
+			rtry += 1
+			_do_search(artist_name, rtry + 1)
+		else:
+			return None
+	except:
+		return None
+	return res
+
 
 artists = json.load(open("data/artists.json","r"))
 total_artists = len(artists)
@@ -31,23 +49,18 @@ t0 = time.time()
 
 artist_list = [a for a in list(artists.keys()) if not (set(a.split()) & {"hits", "greatest", "choir", "collection"})]
 
-for j, artist_name in enumerate(artist_list[57: 60]):
+for j, artist_name in enumerate(artist_list):
 
-	print("artist {}: {}...".format(j+1, artist_name), end="")
+	print("artist {}/{}: {}...".format(j+1, total_artists, artist_name), end="")
 
-	try:
-		res = d.search(artist_name, type="artist") 
-		# discogs_client.models.MixedPaginatedList
-	except HTTPError:
-		print("some http error!!")
-		time.sleep(1)
-		res = d.search(artist_name, type="artist")
-	except:
-		print("some non-http exception")
-		time.sleep(1)
+	res = _do_search(artist_name)
+
+	if not res:
+		print("failed")
 		continue
+
 	if res.count == 0:
-		print("can\'t find")
+		print("failed")
 		continue
 
 	for artist in res.page(1):
@@ -58,22 +71,17 @@ for j, artist_name in enumerate(artist_list[57: 60]):
 		except:
 			continue
 
-	#a = res.page(1)[0]
-
-	print("artist:", a)
-	print(dir(a))
-	print("refresh=", a.images)
-	print("url", a.url)
-	
-	time.sleep(1)
+	time.sleep(SLEEPSEC)
 
 	art_dict = defaultdict()
 	art_dict["name_variations"] = []
+	art_dict["social"] = {}
+	art_dict["albums"] = []
 
 	try:
 		art_dict["name"] = a.name.lower()
 	except:
-		art_dict["name"] = None
+		continue
 
 	try:
 		art_dict["real_name"] = a.real_name.lower()
@@ -90,9 +98,6 @@ for j, artist_name in enumerate(artist_list[57: 60]):
 		art_dict["aliases"] = [al.name.lower() for al in a.aliases]
 	except:
 		art_dict["aliases"] = [None]
-
-	art_dict["social"] = {}
-	art_dict["albums"] = []
 
 	try:
 		for u in a.urls:
@@ -114,30 +119,20 @@ for j, artist_name in enumerate(artist_list[57: 60]):
 				if "other" not in art_dict["social"]:
 					art_dict["social"].update({"other": [u]})
 				else:
-						art_dict["social"]["other"].append(u)
+					art_dict["social"]["other"].append(u)
 	except HTTPError:
-			print("http error - NO URLS!")
+		pass
 	except:
-		print("some non-http problem with urls!")
-
-
-	print("checking releases")
-
-	try:
-		a.releases
-	except:
-		print("NO RELEASES!")
-		print("failed")
-		time.sleep(1)
-		continue
+		pass
 
 	# now browse releases
 	for r in a.releases:
 
 		count_nodata = 0
+
 		try:
 			release_data = r.data
-			time.sleep(1)
+			time.sleep(SLEEPSEC)
 		except AttributeError:
 			count_nodata += 1
 			print("note: {} releases without data so far!", count_nodata)
@@ -146,21 +141,23 @@ for j, artist_name in enumerate(artist_list[57: 60]):
 		if ((release_data["type"] in {"release", "master"}) and
 					(release_data["role"] == "Main") and
 						(len(r.tracklist) > 6)):   # then we assume it's an album
-			print("extracting another album")
 			art_dict["albums"].append(
 				{
 				"title": release_data["title"].lower(),
 				"year": release_data["year"],
-				"tracklist": [{"#": track.data["position"], 
+				"tracklist": [{"#": track.data["position"],
 								"title": track.data["title"].lower(),
 								"duration": track.data["duration"]} for track in r.tracklist]
 				})
 
+	# add this artist's info to the list
 	collected_data.append(art_dict)
-	print("done with this artist")
 
 	print("ok. elapsed time: {:.0f}h {:.0f}m {:.0f}s".format(*_get_time()))
 	# wait to avoid the 'You are making requests too quickly' error
 
-json.dump(collected_data, open(fl, "w"), sort_keys=False, indent=4)
+	tp = divmod(j, SAVE_EVERY)
+
+	if (j > 0) and (tp[1] == 0):
+		json.dump(collected_data[SAVE_EVERY*(tp[0]-1):SAVE_EVERY*tp[0]], open("data/AD{}.json".format(tp[0]), "w"), sort_keys=False, indent=4)
 
