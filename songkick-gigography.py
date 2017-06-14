c3b9e7b2a3091720261cb6553a64158e2d500508
eeghor <eeghor@gmail.com>
2017-06-15 09:52:38 +1000

    still songkick

--- songkick-gigography.py
+++ songkick-gigography.py
@@ -1,25 +1,23 @@
+"""
+Using the Songkick API to collect gigorgaphies
+"""
+
 import requests
 import json
-from bs4 import BeautifulSoup
 
 SONGKICK_API_KEY = "xA3m029StZXk8u3V"
 
 ARTIST = "placebo"
 
+# first, search for an artist - this is needed because we have to have an artist ID before asking for gig data
 search_str = 'http://api.songkick.com/api/3.0/search/artists.json?query=' + ARTIST + '&apikey=' + SONGKICK_API_KEY
-
-print(search_str)
-
 r = requests.get(search_str).text  # response body as bytes so need get text first
 
-res = json.loads(r)["resultsPage"]["results"]["artist"][0]
+res = json.loads(r)["resultsPage"]["results"]["artist"][0]  # take the top search result
 
 artist_id = str(res["id"])
 
 GIG_STR = "http://api.songkick.com/api/3.0/artists/" + artist_id + "/gigography.json?apikey=" + SONGKICK_API_KEY
 r = requests.get(GIG_STR).text
-
-
-#soup = BeautifulSoup(r, "lxml")
 
 print(r)
